b11ac8e347227ec13a310a43fb0a98e38cbe3391
jaisharma010 <jaisharma010@gmail.com>
2021-01-11 00:59:05 +0530

    commit

---
+++ WebApplication1/tipCalculator.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tipCalculator.aspx.cs" Inherits="WebApplication1.tipCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Grapecity</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div style="width: 499px">
+                <br />
+                <asp:Label ID="Label7" runat="server" Text="Choose Currency Type"></asp:Label>
+                <br />
+                <asp:DropDownList ID="DropDownList1" runat="server" ToolTip="Currency Type">
+                    <asp:ListItem Value="US">US Dollars</asp:ListItem>
+                    <asp:ListItem>INR</asp:ListItem>
+                    <asp:ListItem Value="SD">Singapore Dollars</asp:ListItem>
+                    <asp:ListItem Value="Eu">Euro</asp:ListItem>
+                </asp:DropDownList>
+                <br />
+                <br />
+            <asp:Label ID="Label1" runat="server" Text="BIll"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" OnTextChanged="tip_Calculator" ToolTip="Bill" CausesValidation="True"></asp:TextBox>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="Label6" runat="server" Enabled="False" ForeColor="Red"></asp:Label>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="Bill Amount Should be specified" ForeColor="Red"></asp:RequiredFieldValidator>
+                <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Tip %"></asp:Label>
+            <br />
+                 <asp:TextBox ID="TextBox2" runat="server" min="0" max="1000" step="10" value="10" AutoPostBack="True" OnTextChanged="tip_Calculator" TextMode="Number" ToolTip="Tip"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Number of people"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox3" runat="server" style="margin-bottom: 0px" TextMode="Number" min="1" value="1" AutoPostBack="True" OnTextChanged="tip_Calculator" ></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+           </div>
+            <div>
+
+                <asp:Label ID="Label4" runat="server" Text="Tip &lt;br&gt; Per Person"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="TextBox4" runat="server" Enabled="False" ReadOnly="True"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Label ID="Label5" runat="server" Text="Total"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="TextBox5" runat="server" Enabled="False" ReadOnly="True"></asp:TextBox>
+
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/tipCalculator.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    interface calculation
+    {
+       void calculation();
+    }
+    interface currency
+    {
+        void currency();
+    }
+    public abstract partial class tipCalculator : System.Web.UI.Page,calculation,currency
+    {
+       public int ptip,bill, tip, person,error=0; //global integer values
+       public string curr; //global string values
+
+        public void tip_Calculator(object sender, EventArgs e)
+        {
+            //Input checked for wrong data values
+            try
+            {
+                bill = Convert.ToInt32(TextBox1.Text);
+                tip = Convert.ToInt32(TextBox2.Text);
+                person = Convert.ToInt32(TextBox3.Text);
+                curr = DropDownList1.SelectedValue; //currency choosen by user
+            }
+            catch
+            {
+                //flag for any error
+                error = 1;
+                if (error == 1)
+                {
+                    Label6.Text = "Please enter valid data!";      //error messsage
+                    Label6.Enabled = true;
+                    error = 0;
+                }
+                return ; //stoping process after error is found
+            }
+            if (bill <= 0)
+            {
+                Label6.Text = "BIll amount should be more than 0"; //error messsage
+                return; //stoping process after error is found
+            }
+            if (error == 0)
+                Label6.Text = null;
+
+            if (tip < 0)
+            {
+                Label6.Text = "Tip cannot be less than 0%";  //error messsage
+                return; //stoping process after error is found
+            }
+
+            if (person <= 0)
+            {
+                Label6.Text = "Number of persons cannot be 0"; //error messsage
+                return ; //stoping process after error is found
+            }
+            currency();
+         //calling function to choosing appropriate currency
+        }
+        public void currency()
+        {
+            switch (curr) //deciding which currency symbol to use
+            {
+                case "INR":
+                    curr = "\u20B9";
+                    break;
+
+                case "Eu":
+                    curr = "\u20AC";
+                    break;
+                case "US":
+                    curr = "$";
+                    break;
+                case "SD":
+                    curr = "$";
+                    break;
+                default:
+                    curr = "default called";
+                    break;
+            }
+            calculation(); //calling calculation method
+        }
+        public void calculation()
+        {
+            ptip = ((bill * tip) / 100) / person; //calculating tip per person
+            TextBox4.Text = curr + (ptip.ToString()); //printing tip per person, also concatinating currency symbol
+            TextBox5.Text = curr + ((bill / person) + ptip).ToString(); //printing total bill per person including tip
+        }
+    }
+}
